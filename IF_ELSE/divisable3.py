f7e40551f2d3adef86e68db3fc9e0c04954c2a3f
sudeepgurjar <sudeepgurjar6111@gmail.com>
2023-02-14 16:21:42 +0530

    marks

---
+++ IF_ELSE/divisable3.py
@@ -0,0 +1,6 @@
+
+x=int(input("enter a number:"))
+if x%3 == 0:
+ print("the number is divisble by 3")
+else:
+ 	print("not")
---
+++ IF_ELSE/marks.py
@@ -0,0 +1,16 @@
+
+#Prectice question Test-2
+
+
+#Q1.wite a program to accept persentage from the user and displye the frade according to the following ceiteia.
+
+
+m=int(input("enter a marks:"))
+if m >= 90:
+  print("A") 
+elif m >80 and m<= 90:
+  print("B")
+elif m >=60 and m<= 80:
+  	print("C")
+else:
+	print("D")
